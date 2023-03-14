61abab4dfe3d825d7699aee9dcb1231d7606d75e
samuel-boobier <61465174+samuel-boobier@users.noreply.github.com>
2023-03-14 14:12:47 +0000

    dataset and target value ranges

--- ML_MOFs/Analysis/analysis_methods.py
+++ ML_MOFs/Analysis/analysis_methods.py
@@ -3,6 +3,8 @@ import pandas as pd
 from plotly.subplots import make_subplots
 import math
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
 
 
 # calculate the min, max, mean, median, and std of data
@@ -13,7 +15,6 @@ def data_analysis_table(df):
                 np.std(df[desc].tolist())]
         stats.append(temp)
     stats = pd.DataFrame(data=stats, columns=["Descriptor", "Minimum", "Maximum", "Mean", "Median", "Std Dev"])
-    stats = stats.round(3)
     return stats
 
 
@@ -25,8 +26,9 @@ def range_subplot(df, file_name):
     for n in range(len(title_names)):
         if len(title_names[n]) > 25:
             title_names[n] = title_names[n][0:24]
-    fig = make_subplots(rows=math.ceil(len(descriptor_names) / 2), cols=2, subplot_titles=title_names,
-                        horizontal_spacing=0.06, vertical_spacing=0.12)
+
+    fig = make_subplots(rows=math.ceil(len(descriptor_names) / 2), cols=math.ceil(len(descriptor_names) / 2),
+                        subplot_titles=title_names, horizontal_spacing=0.06, vertical_spacing=0.12)
     for desc in range(len(descriptor_names)):
         if (desc + 1) % 2 == 1:
             fig.add_trace(go.Histogram(x=df[descriptor_names[desc]], marker=dict(color="#1f77b4")),
@@ -34,8 +36,72 @@ def range_subplot(df, file_name):
         if (desc + 1) % 2 == 0:
             fig.add_trace(go.Histogram(x=df[descriptor_names[desc]], marker=dict(color="#1f77b4")),
                           row=math.ceil((desc + 1) / 2), col=2)
-    fig.update_layout(width=600, height=600, showlegend=False, margin=dict(l=10, r=10, t=20, b=10))
+    if len(descriptor_names) > 2:
+        w = 600
+        h = 600
+    elif len(descriptor_names) == 2:
+        w = 600
+        h = 300
+    else:
+        w = 300
+        h = 300
+    fig.update_layout(width=w, height=h, showlegend=False, margin=dict(l=10, r=10, t=20, b=10))
     fig.update_traces(hovertemplate="Range: %{x}<br>Frequency: %{y}<extra></extra>")
     fig.update_annotations(font_size=11)
     filename = "../Graphs/Analysis_graphs/" + file_name + ".png"
     fig.write_image(filename, scale=2)
+
+
+def corr_graph(Data, title, file_name):
+    # get correlation table
+    corr = Data.corr()
+    # convert to R^2
+    corr = corr.pow(2)
+    col_names = Data.dtypes.index
+    fig = plt.figure(figsize=(9, 9))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(corr, cmap='Reds', vmin=0, vmax=1)
+    cb = fig.colorbar(cax, shrink=0.825)
+    cb.ax.tick_params(labelsize=9)
+    ticks = np.arange(0, len(Data.columns), 1)
+    ax.set_xticks(ticks)
+    # plt.xticks(rotation=90)
+    ax.set_yticks(ticks)
+    ax.set_xticklabels(col_names, fontsize=8)
+    ax.set_yticklabels(col_names, fontsize=8)
+    ax.xaxis.set_tick_params(rotation=90)
+    plt.ylim(len(Data.columns)-0.5, -0.5)
+    # y parameter move title up to be legible
+    ax.set_title(title, y=1.15, fontsize=12)
+    # Find descriptors with R^2
+    corr = Data.corr()
+    corr = corr.pow(2)
+    # using numpy, get values
+    arr = corr.values
+    index_names = corr.index
+    col_names = corr.columns
+
+    #  Get indices where such threshold is crossed; avoid diagonal elems
+    R, C = np.where(np.triu(arr, 1) > 0)
+    # Arrange those in columns and put out as a dataframe
+    out_arr = np.column_stack((index_names[R], col_names[C], arr[R, C]))
+    df_corr = pd.DataFrame(out_arr, columns=['row_name', 'col_name', 'R2'])
+    df_corr = df_corr.sort_values(by='R2', ascending=False)
+    plt.tight_layout()
+    filename = "../Graphs/Analysis_graphs/" + file_name + ".png"
+    plt.savefig(filename, dpi=600)
+    return df_corr
+
+
+def plot(n1, n2, data, name):
+    plt.figure(figsize=(4.5, 4.5))
+    x = list(data[n1])
+    x = [float(i) for i in x]
+    y = list(data[n2])
+    y = [float(i) for i in y]
+    plt.scatter(x, y, color="black", s=5)
+    plt.title(n1 + " vs " + n2)
+    plt.xlabel(n1)
+    plt.ylabel(n2)
+    plt.tight_layout()
+    plt.savefig("../Graphs/Analysis_graphs/" + name + ".png", dpi=600)
--- ML_MOFs/Analysis/data_analysis.py
+++ ML_MOFs/Analysis/data_analysis.py
@@ -1,26 +1,63 @@
 import pandas as pd
-from analysis_methods import data_analysis_table, range_subplot
+from numpy import log10, floor
+from analysis_methods import data_analysis_table, range_subplot, corr_graph, plot
 
+pd.set_option('display.precision', 2)
 # Explore the ranges and correlation of the initial descriptor set
 
 # load data
 df_data = pd.read_csv("../Data/MOF_data.csv")
-target_descriptors = df_data.drop(columns=["MOF", "CO2 error (mol/kg)", "CH4 error (mol/kg)", "Selectivity (CO2)",
-                                           "Selectivity error", "logS error", "TSN error", "logS", "TSN Class",
-                                           "Maximum Dimensions"])
+targets = ["CO2 loading (mol/kg)", "CH4 loading (mol/kg)", "TSN", "LOG10 TSN"]
+initial_descriptors = ["PLD", "LCD", "Density (g/cc)", "VSA (m2/cc)", "GSA (m2/g)", "VF", "PV (cc/g)", "K0_CH4",
+                       "K0_CO2", "K0_H2S", "K0_H2O", "DC_CH4", "DC_CO2", "DC_H2S", "P_CH4", "P_CO2", "P_H2S", "Qst_CH4",
+                       "Qst_CO2", "Qst_H2S", "Qst_H2O"]
+
+log10_descriptors = ["PLD log10", "LCD log10", "PV (cc/g) log10", "K0_CH4 log10", "K0_CO2 log10", "K0_H2S log10",
+                     "K0_H2O log10", "DC_CH4 log10", "DC_CO2 log10", "DC_H2S log10", "P_CH4 log10", "P_CO2 log10",
+                     "P_H2S log10"]
+final_descriptors = ["PLD (log10)", "LCD (log10)", "Density (g/cc)", "VSA (m2/cc)", "VF", "DC_CH4 (log10)",
+                     "DC_CO2 (log10)", "DC_H2S (log10)", "Qst_CH4", "Qst_CO2", "Qst_H2S", "Qst_H2O"]
+target_descriptors = df_data[targets + initial_descriptors + log10_descriptors]
 
 # Descriptor and target stats
 descriptor_stats = data_analysis_table(target_descriptors)
 descriptor_stats.to_csv("../Results/Analysis_results/dataset_analysis.csv")
+print(descriptor_stats.to_latex(index=False))
 
 # target ranges
-targets = df_data[["CO2 loading (mol/kg)", "CH4 loading (mol/kg)", "TSN", "LOG10 TSN"]]
-range_subplot(targets, "target_ranges")
+df_targets = df_data[targets]
+range_subplot(df_targets, "target_ranges")
 
 # Initial descriptor ranges
+df_descriptors = target_descriptors[initial_descriptors]
+descriptors = list(df_descriptors)
+desc_chunks = [descriptors[i:i+4] for i in range(0, len(descriptors), 4)]
+for i in range(len(desc_chunks)):
+    range_subplot(df_descriptors[desc_chunks[i]], "initial_descriptor_ranges_" + str(i))
 
 # Initial descriptor ranges after log10 transformations
+# PLD, LCD, PV, K0s, DCs, and Ps changed to log10
+df_descriptors_log10 = df_data[log10_descriptors]
+descriptors = list(log10_descriptors)
+desc_chunks = [descriptors[i:i+4] for i in range(0, len(descriptors), 4)]
+for i in range(len(desc_chunks)):
+    range_subplot(df_descriptors_log10[desc_chunks[i]], "log10_descriptor_ranges_" + str(i))
 
 # Initial descriptor correlation Analysis
+descriptor_corr = corr_graph(df_data[initial_descriptors + log10_descriptors], "Correlation Initial Descriptors",
+                             "corr_plot")
+descriptor_corr.to_csv("../Results/Analysis_results/descriptor_correlation.csv")
+
+# descriptor plots
+plot("PV (cc/g) log10", "VF", df_data, "PV_VF")
+plot("GSA (m2/g)", "VF", df_data, "GSA_VF")
+plot("K0_CH4 log10", "Qst_CH4", df_data, "K0_Qst_CH4")
+plot("K0_CO2 log10", "Qst_CO2", df_data, "K0_Qst_CO2")
+plot("K0_H2O log10", "Qst_H2O", df_data, "K0_Qst_H2O")
+plot("K0_H2S log10", "Qst_H2S", df_data, "K0_Qst_H2S")
 
 # Final descriptors
+# PV (cc/g) log10 removed due to correlation with VF
+# GSA (m2/g) removed due to similarity with VF
+# K0s removed due to similarity to Qsts
+# Ps removed as deemed less relevant
