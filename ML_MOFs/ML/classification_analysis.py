b6e168cc731705ae552606ad7488138ca1cc2757
Samuel Boobier <samuel.boobier@gmail.com>
2024-01-23 17:46:54 +0000

    updated test classification analysis results

--- ML_MOFs/ML/classification_analysis.py
+++ ML_MOFs/ML/classification_analysis.py
@@ -7,9 +7,9 @@ import numpy as np
 # plot histogram of HIGH and LOW predictions compared to TSN
 def get_classification_histogram(method):
     # change here for training results
-    prediction_data = pd.read_csv("../Results/ML_results/Classification/" + method + "_predictions.csv")
+    prediction_data = pd.read_csv("../Results/ML_results/Test_set/" + method + "_classification_predictions.csv")
     prediction_data = prediction_data.sort_values(by="MOF")
-    data = pd.read_csv("..\\Data\\MOF_data.csv")
+    data = pd.read_csv("..\\Data\\MOF_data_test.csv")
     data = data.sort_values(by="MOF")
     prediction_data["TSN"] = data["TSN"].tolist()
     fig = px.histogram(prediction_data, x="TSN", color="TSN Class Prediction", barmode="overlay", width=400, height=200)
@@ -34,7 +34,7 @@ def get_classification_histogram(method):
         title_standoff=3
     )
     # change here for training results
-    filename = "../Graphs/ML_graphs/Classification/" + method + "_histogram.png"
+    filename = "../Results/ML_results/Test_set/" + method + "_histogram.png"
     fig.write_image(filename, scale=2)
 
 
@@ -46,7 +46,7 @@ get_classification_histogram("KNN")
 # roc plots
 def roc_plot(method):
     # change here for training results
-    df_roc = pd.read_csv("../Results/ML_results/Classification/" + method + "_roc.csv")
+    df_roc = pd.read_csv("../Results/ML_results/Test_set/" + method + "_roc.csv")
 
     fpr = df_roc["False Positive Rate"].tolist()
     tpr = df_roc["True Positive Rate"].tolist()
@@ -93,7 +93,7 @@ def roc_plot(method):
                      range=[0, 1]
                      )
     fig.update_traces(line=dict(width=1))
-    filename = "../Graphs/ML_graphs/Classification/" + method + "_roc_curve.png"
+    filename = "../Results/ML_results/Test_set/" + method + "_roc_curve.png"
     print(auc(fpr, tpr))
     fig.write_image(filename, scale=2)
 
@@ -108,7 +108,7 @@ target = "TSN Class"
 
 def probability_plots(method):
     # change here for training results
-    df = pd.read_csv("../Results/ML_results/Classification/" + method + "_predictions.csv")
+    df = pd.read_csv("../Results/ML_results/Test_set/" + method + "_classification_predictions.csv")
     # The histogram of scores compared to true labels
     fig = px.histogram(df, x="HIGH Probability", color="TSN Class", nbins=50, width=400, height=200,
                        labels=dict(color='True Labels', x='Score'), barmode="overlay")
@@ -134,7 +134,7 @@ def probability_plots(method):
         title_standoff=3,
     )
     # change here for training results
-    filename = "../Graphs/ML_graphs/Classification/" + method + "_probs_histogram.png"
+    filename = "../Results/ML_results/Test_set/" + method + "_probs_histogram.png"
     fig.write_image(filename, scale=2)
 
 
@@ -157,37 +157,37 @@ descs = {
 
 
 # change here for training results
-data = pd.read_csv("..\\Results\\ML_results\\Classification\\RF_importance.csv")
-data = data.sort_values(by="TSN Class Mean", ascending=False)
-fig = px.bar(data, x='Descriptor', y='TSN Class Mean', error_y='TSN Class SD', color_discrete_sequence=[px.colors.
-             qualitative.D3[0]])
-fig.update_xaxes(
-    tickangle=90,
-    mirror=True,
-    ticks='outside',
-    showline=True,
-    linecolor='black',
-    title_standoff=5,
-)
-fig.update_yaxes(
-    mirror=True,
-    ticks='outside',
-    showline=True,
-    linecolor='black',
-    title_standoff=3,
-)
-fig.update_layout(barmode='group',
-                  xaxis_title=None,
-                  width=200,
-                  height=200,
-                  xaxis=dict(showgrid=False, tickvals=np.arange(0, len(descs)), ticktext=[descs[x] for x in
-                                                                                          data["Descriptor"]]),
-                  yaxis=dict(showgrid=False),
-                  yaxis_title="Importance",
-                  plot_bgcolor='white',
-                  margin=dict(l=10, r=10, t=10, b=0),
-                  )
-fig.data[0].error_y.thickness = 1
-fig.data[0].error_y.width = 2
-filename = "../Graphs/ML_graphs/Classification/RF_importance.png"
-fig.write_image(filename, scale=2)
+# data = pd.read_csv("..\\Results\\ML_results\\Classification\\RF_importance.csv")
+# data = data.sort_values(by="TSN Class Mean", ascending=False)
+# fig = px.bar(data, x='Descriptor', y='TSN Class Mean', error_y='TSN Class SD', color_discrete_sequence=[px.colors.
+#              qualitative.D3[0]])
+# fig.update_xaxes(
+#     tickangle=90,
+#     mirror=True,
+#     ticks='outside',
+#     showline=True,
+#     linecolor='black',
+#     title_standoff=5,
+# )
+# fig.update_yaxes(
+#     mirror=True,
+#     ticks='outside',
+#     showline=True,
+#     linecolor='black',
+#     title_standoff=3,
+# )
+# fig.update_layout(barmode='group',
+#                   xaxis_title=None,
+#                   width=200,
+#                   height=200,
+#                   xaxis=dict(showgrid=False, tickvals=np.arange(0, len(descs)), ticktext=[descs[x] for x in
+#                                                                                           data["Descriptor"]]),
+#                   yaxis=dict(showgrid=False),
+#                   yaxis_title="Importance",
+#                   plot_bgcolor='white',
+#                   margin=dict(l=10, r=10, t=10, b=0),
+#                   )
+# fig.data[0].error_y.thickness = 1
+# fig.data[0].error_y.width = 2
+# filename = "../Graphs/ML_graphs/Classification/RF_importance.png"
+# fig.write_image(filename, scale=2)
